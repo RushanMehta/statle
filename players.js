9241f997017f634b6c0e64ec50160563739d2389
RushanMehta <meht9068@wths.net>
2026-07-19 11:47:41 -0500

    Create players.js

---
+++ players.js
@@ -0,0 +1,14 @@
+const playersDatabase = {
+    football: [
+        { name: "Patrick Mahomes", position: "QB", team: "KC", yards: 3735, tds: 22, jersey: 15, age: 30 },
+        { name: "Lamar Jackson", position: "QB", team: "BAL", yards: 2898, tds: 23, jersey: 8, age: 29 },
+        { name: "Christian McCaffrey", position: "RB", team: "SF", yards: 2126, tds: 17, jersey: 23, age: 30 },
+        { name: "Justin Jefferson", position: "WR", team: "MIN", yards: 1320, tds: 8, jersey: 18, age: 27 },
+        { name: "Travis Kelce", position: "TE", team: "KC", yards: 945, tds: 6, jersey: 87, age: 36 }
+        // Add all other 2025-2026 skill players here following this exact format!
+    ],
+    basketball: [],
+    baseball: [],
+    hockey: [],
+    soccer: []
+};
